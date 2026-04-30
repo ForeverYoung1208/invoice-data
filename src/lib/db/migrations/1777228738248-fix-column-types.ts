67767ea6cdf0544b8bdf0cf3e9380cb36be8887c
Ihor S <siafin2010@gmail.com>
2026-04-30 17:45:15 +0200

    format migration file

--- src/lib/db/migrations/1777228738248-fix-column-types.ts
+++ src/lib/db/migrations/1777228738248-fix-column-types.ts
@@ -1,20 +1,31 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class FixColumnTypes1777228738248 implements MigrationInterface {
-    name = 'FixColumnTypes1777228738248'
+  name = 'FixColumnTypes1777228738248';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "task_files" DROP COLUMN "filePath"`);
-        await queryRunner.query(`ALTER TABLE "task_files" ADD "filePath" character varying(2048) NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "task_files" DROP COLUMN "originalName"`);
-        await queryRunner.query(`ALTER TABLE "task_files" ADD "originalName" character varying(2048) NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "task_files" DROP COLUMN "filePath"`);
+    await queryRunner.query(
+      `ALTER TABLE "task_files" ADD "filePath" character varying(2048) NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "task_files" DROP COLUMN "originalName"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "task_files" ADD "originalName" character varying(2048) NOT NULL`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "task_files" DROP COLUMN "originalName"`);
-        await queryRunner.query(`ALTER TABLE "task_files" ADD "originalName" character varying NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "task_files" DROP COLUMN "filePath"`);
-        await queryRunner.query(`ALTER TABLE "task_files" ADD "filePath" character varying NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "task_files" DROP COLUMN "originalName"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "task_files" ADD "originalName" character varying NOT NULL`,
+    );
+    await queryRunner.query(`ALTER TABLE "task_files" DROP COLUMN "filePath"`);
+    await queryRunner.query(
+      `ALTER TABLE "task_files" ADD "filePath" character varying NOT NULL`,
+    );
+  }
 }
